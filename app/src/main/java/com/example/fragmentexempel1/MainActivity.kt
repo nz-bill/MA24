1d23a3bf120559af0475012d00e95e4a8b8dbecb
nz-bill <bill.palmestedt@zocom.se>
2024-11-18 15:30:25 +0100

    initial commit

---
+++ app/src/main/java/com/example/fragmentexempel1/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.fragmentexempel1
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fragmentexempel1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    val firstFragment= FirstFragment()
+    val secondFragment = SecondFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+//         ---------tre sätt att använda fragmentManager----------
+
+        // metod 1: skapa referens till begintransaction()
+//        val transaction =supportFragmentManager.beginTransaction()
+//        transaction.add(R.id.fc_container,firstFragment)
+//        transaction.commit()
+
+        //metod 2: gör alla operationer på en rad
+  //      supportFragmentManager.beginTransaction().add(R.id.fc_container,firstFragment).commit()
+
+
+        //metod 3: använd Apply metoden för att 'gruppera' operationerna
+//        supportFragmentManager.beginTransaction().apply {
+//
+//            add(R.id.fc_container, firstFragment )
+//            commit()
+//
+//        }
+
+        binding.btnFirst.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+
+                replace(R.id.fc_container, firstFragment )
+
+                //addToBackStack gör så att vi kan använda 'back' knappen i android för att gå tillbaka till föregående vy
+                addToBackStack("frag1")
+                commit()
+
+            }
+        }
+
+
+        binding.btnSecond.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+
+                // skapa en bundle som innehåller key-value par som vi vill skicka med fragmentet
+                val myBundle = Bundle()
+                myBundle.apply {
+                    putString("bodyText", "Jag är text från en bundle")
+
+                }
+
+                //uppdatera secondFragment med nya argument (bundle)
+                secondFragment.arguments = myBundle
+
+                //byter ut fragment i vår fragmentManager
+                replace(R.id.fc_container, secondFragment)
+                addToBackStack("frag2")
+                commit()
+            }
+        }
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fragmentexempel1/SecondFragment.kt
@@ -0,0 +1,75 @@
+package com.example.fragmentexempel1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+
+    //dessa används ej i detta exempel
+//    // TODO: Rename and change types of parameters
+//    private var param1: String? = null
+//    private var param2: String? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_second, container, false )
+
+        val textView = view.findViewById<TextView>(R.id.tv_text)
+
+        val text = arguments?.getString("bodyText") ?: "no text found"
+
+        textView.text = text
+
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_second, container, false)
+
+        //vi returnerar en view EFTER att vi uppdaterat den
+        return view
+    }
+
+
+    //dessa används ej i detta exempel
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment SecondFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            SecondFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
+}
