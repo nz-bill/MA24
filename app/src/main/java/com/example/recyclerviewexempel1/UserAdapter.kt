a1b07b807e638c1eb897067c462c7c5462394d16
nz-bill <bill.palmestedt@zocom.se>
2024-11-25 15:02:13 +0100

    added comments to UserAdapter

--- app/src/main/java/com/example/recyclerviewexempel1/UserAdapter.kt
+++ app/src/main/java/com/example/recyclerviewexempel1/UserAdapter.kt
@@ -8,20 +8,24 @@ import androidx.recyclerview.widget.RecyclerView
 
 class UserAdapter(val users: MutableList<String>) : RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
 
+    // en ViewHolder håller referenser till vyerna i varje 'rad'
    class UserViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
        val nameTextView: TextView = itemView.findViewById(R.id.tv_name)
 
    }
 
+    // Skapar en ny 'rad' i vår recyclerview (ViewHolder)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item,parent,false)
         return UserViewHolder(view)
     }
 
+    // Returnerar antal objekt i listan
     override fun getItemCount(): Int {
         return users.size
     }
 
+    // Binder data till varje ViewHolder. Varje element position i listan har samma position i recyclerview
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
        holder.nameTextView.text = users[position]
     }
